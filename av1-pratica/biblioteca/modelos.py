0d9607e65d4031452c37fce64e665d96e48ed298
Tonidev <antoniodev2709@gmail.com>
2026-09-04 22:55:09 -0300

    feat: add book and loan models

--- av1-pratica/biblioteca/modelos.py
+++ av1-pratica/biblioteca/modelos.py
@@ -1,2 +1,20 @@
+from dataclasses import dataclass
+from datetime import date
 
-    
+
+@dataclass
+class Livro:
+    id: str
+    titulo: str
+    autor: str
+    categoria: str
+    quantidade: int
+    quantidade_total: int
+
+
+@dataclass
+class Emprestimo:
+    usuario_id: str
+    livro_id: str
+    vencimento: date
+    devolvido: bool = False
