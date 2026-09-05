98a262bdc905827459a75ba84d9223ed9a9f7ee1
Tonidev <antoniodev2709@gmail.com>
2026-09-04 23:09:52 -0300

    feat: add employee class

--- av1-pratica/biblioteca/usuarios.py
+++ av1-pratica/biblioteca/usuarios.py
@@ -52,3 +52,18 @@ class UsuarioPremium(Usuario):
     @property
     def multa_por_dia(self):
         return 1
+
+@dataclass
+class Funcionario(Usuario):
+
+    @property
+    def limite_emprestimos(self):
+        return 10
+
+    @property
+    def prazo_emprestimo(self):
+        return 30
+
+    @property
+    def multa_por_dia(self):
+        return 0
